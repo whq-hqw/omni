785e224502bf6a2761a86e2c167aadee5e5268f4
loveorchids <loveorchidsdavid@gmail.com>
2018-08-26 18:29:48 +0900

    new

--- network/resnet.py
+++ network/resnet.py
@@ -1,45 +1,64 @@
 import tensorflow as tf
 import numpy as np
+import os
 
-def block_conv_1(input):
+sess = tf.Session()
+
+a = tf.placeholder(tf.float16, shape = (None, 224, 224, 3), name="img")
+
+def resnet(input, layers = 50):
+    NET_DEF = {50:[3, 4, 6, 3], 101: [3, 4, 23, 3], 152: [3, 8, 36, 3]}
+
     with tf.variable_scope("conv1_x"):
-        return tf.layers.conv2d(inputs=input, filters=64, kernel_size=7, padding="same", activation=tf.nn.relu)
+        conv = tf.layers.conv2d(inputs=input, filters=64, kernel_size=7, padding="same", activation=tf.nn.relu)
+        net = tf.layers.batch_normalization(conv)
 
-def block_con2x(input, repeat=3):
     with tf.variable_scope("conv2_x"):
-        tmp =  tf.layers.max_pooling2d(inputs=input, pool_size=[3,3], strides=2)
-        for _ in range(repeat):
-            tmp = tf.layers.conv2d(inputs=tmp, filters=64, kernel_size=1, padding="same", activation=tf.nn.relu)
-            tmp = tf.layers.conv2d(inputs=tmp, filters=128, kernel_size=3, padding="same", activation=tf.nn.relu)
-            tmp = tf.layers.conv2d(inputs=tmp, filters=256, kernel_size=1, padding="same", activation=tf.nn.relu)
-    return tmp
+        net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
+        for _ in range(NET_DEF[layers][0]):
+            conv = tf.layers.conv2d(inputs=net, filters=64, kernel_size=1, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            conv = tf.layers.conv2d(inputs=conv, filters=128, kernel_size=3, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            conv = tf.layers.conv2d(inputs=conv, filters=256, kernel_size=1, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            net = tf.add(conv, net)
 
-def block_con3x(input, repeat=4):
-    tmp = input
     with tf.variable_scope("conv3_x"):
-        for _ in range(repeat):
-            tmp = tf.layers.conv2d(inputs=tmp, filters=128, kernel_size=1, padding="same", activation=tf.nn.relu)
-            tmp = tf.layers.conv2d(inputs=tmp, filters=128, kernel_size=3, padding="same", activation=tf.nn.relu)
-            tmp = tf.layers.conv2d(inputs=tmp, filters=256, kernel_size=1, padding="same", activation=tf.nn.relu)
-    return tmp
+        for _ in range(NET_DEF[layers][1]):
+            conv = tf.layers.conv2d(inputs=net, filters=128, kernel_size=1, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            conv = tf.layers.conv2d(inputs=conv, filters=128, kernel_size=3, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            conv = tf.layers.conv2d(inputs=conv, filters=256, kernel_size=1, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            net = tf.add(conv, net)
 
-def block_con4x(input, repeat=6):
-    tmp = input
     with tf.variable_scope("conv4_x"):
-        for _ in range(repeat):
-            tmp = tf.layers.conv2d(inputs=tmp, filters=256, kernel_size=1, padding="same", activation=tf.nn.relu)
-            tmp = tf.layers.conv2d(inputs=tmp, filters=256, kernel_size=3, padding="same", activation=tf.nn.relu)
-            tmp = tf.layers.conv2d(inputs=tmp, filters=1024, kernel_size=1, padding="same", activation=tf.nn.relu)
-    return tmp
+        for _ in range(NET_DEF[layers][2]):
+            conv = tf.layers.conv2d(inputs=net, filters=256, kernel_size=1, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            conv = tf.layers.conv2d(inputs=conv, filters=256, kernel_size=3, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            conv = tf.layers.conv2d(inputs=conv, filters=1024, kernel_size=1, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            net = tf.add(conv, net)
 
-def block_con5x(input, repeat=3):
-    tmp = input
     with tf.variable_scope("conv5_x"):
-        for _ in range(repeat):
-            tmp = tf.layers.conv2d(inputs=tmp, filters=512, kernel_size=1, padding="same", activation=tf.nn.relu)
-            tmp = tf.layers.conv2d(inputs=tmp, filters=512, kernel_size=3, padding="same", activation=tf.nn.relu)
-            tmp = tf.layers.conv2d(inputs=tmp, filters=2048, kernel_size=1, padding="same", activation=tf.nn.relu)
-    return tmp
+        for _ in range(NET_DEF[layers][3]):
+            conv = tf.layers.conv2d(inputs=net, filters=512, kernel_size=1, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            conv = tf.layers.conv2d(inputs=conv, filters=512, kernel_size=3, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            conv = tf.layers.conv2d(inputs=conv, filters=2048, kernel_size=1, padding="same", activation=tf.nn.relu)
+            conv = tf.layers.batch_normalization(conv)
+            net = tf.add(conv, net)
+
+    with tf.variable_scope("average_pool"):
+        net = tf.layers.dense(net, 1000, activation=tf.nn.softmax)
+    return net
 
 
-a = tf.layers.dense(inputs=1)
+net = resnet(a)
+result = sess.run(net, feed_dict={a:np.random.normal(size=(1, 224, 224, 3))})
+
