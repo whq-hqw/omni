932d16ddfa7ae8da3b793616698a63861dd9cb31
loveorchids <loveorchidsdavid@gmail.com>
2018-09-12 19:07:15 +0900

    start

--- datasets/load_data.py
+++ datasets/load_data.py
@@ -1,38 +1,28 @@
 import tensorflow as tf
 import os, random
 
-class Sample():
-    def __init__(self, label, path):
-        assert path is not None, "Path should not be None"
-        self.label = label
-        self.path = path
-
-    def __str__(self):
-        return self.path + ": " + self.label
-    
-
 def extension_check(file, extentions):
     if file[file.rfind(".")+1:] in extentions:
         return True
     else:
         return False
 
-def get_ilsvrc_dataset(path, extensions=set(["jpg", "jpeg", "JPG", "png", "PNG", "bmp", "BMP"]), verbose=False):
-    dataset = []
+def ilsvrc_dataset(path, extensions=set(["jpg", "jpeg", "JPG", "png", "PNG", "bmp", "BMP"]), verbose=False):
+    dataset = {}
     path = os.path.expanduser(path)
     classes = os.listdir(path)
     classes = [cls for cls in classes if os.path.isdir(os.path.join(path, cls))]
-    for cls in classes:
+    for i, cls in enumerate(classes):
         if verbose:
-            print('Loading data, Processing {}'.format(cls))
-        dataset += [Sample(label=cls, path=_) for _ in os.listdir(os.path.join(path, cls)) if
-                    extension_check(_, extensions)]
+            print('Loading {}th {} class.'.format(i, cls))
+        dataset.update({cls: [_ for _ in os.listdir(os.path.join(path, cls)) if
+                    extension_check(_, extensions)]})
     print("Dataset loading is complete.")
     return dataset
 
-def get_img2img_dataset(path, A_B_folder=["trainA", "trainB"], one_to_one=True,
+def img2img_dataset(path, A_B_folder=["trainA", "trainB"], one_to_one=True,
                         extensions=set(["jpg", "jpeg", "JPG", "png", "PNG", "bmp", "BMP"]), verbose=False):
-    dataset = []
+    dataset = {}
     path = os.path.expanduser(path)
     assert len(A_B_folder) == 2, "A_B_folder should be the name of source and target folder."
     source = os.path.join(path, A_B_folder[0])
@@ -49,15 +39,15 @@ def get_img2img_dataset(path, A_B_folder=["trainA", "trainB"], one_to_one=True,
         for i in range(len(source_imgs)):
             if verbose and i % 100 == 0:
                 print("{} samples has been loaded...".format(i))
-            dataset.append(Sample(label=target_imgs[i], path=source_imgs[i]))
+            dataset.update({target_imgs[i]: source_imgs[i]})
     else:
-        dataset.append([Sample(label="A", path=_) for _ in os.listdir(source) if extension_check(_, extensions)])
-        print("Source data loaded.")
-        dataset.append([Sample(label="B", path=_) for _ in os.listdir(target) if extension_check(_, extensions)])
+        dataset.update({"A": [_ for _ in os.listdir(source) if extension_check(_, extensions)]})
+        dataset.update({"B": [_ for _ in os.listdir(target) if extension_check(_, extensions)]})
+        #dataset.append([Sample(label="B", path=_) for _ in os.listdir(target) if extension_check(_, extensions)])
     print("Dataset loading is complete.")
     return dataset
 
-def get_dataset_with_addtional_info(path, extensions=None, verbose=False):
+def dataset_with_addtional_info(path, extensions=None, verbose=False):
     # TODO: Implement this dataload method
     dataset = []
     path = os.path.expanduser(path)
@@ -96,8 +86,8 @@ def data_load_graph(img_path, ground_truth, threads, batch_size, output_shape, c
     return enqueue_op, image_batch, label_batch
 
 if __name__ == "__main__":
-    #dataset = get_ilsvrc_dataset(path="~/Downloads/Dataset/pedestrain")
+    #dataset = ilsvrc_dataset(path="~/Downloads/Dataset/pedestrain")
     #print(len(dataset))
     
-    dataset = get_img2img_dataset(path="~/Pictures/dataset/buddha")
+    dataset = img2img_dataset(path="~/Pictures/dataset/buddha")
     print(len(dataset))
--- networks/recognition/resnet.py
+++ networks/recognition/resnet.py
@@ -1,14 +1,16 @@
+import os
 import tensorflow as tf
 import networks.blocks as block
 import datasets.load_data as load
 from networks.train_op import build_train_op
 
 class Resnet():
-    def __init__(self, args):
+    def __init__(self, args, path):
         # Declare Placeholders
         self.image_paths_placeholder = tf.placeholder(shape=(None), dtype=tf.string)
         self.ground_truth_placeholder = tf.placeholder(shape=(None), dtype=tf.int32)
         self.num_epoch = args.epoch_num
+        self.dataset_path = os.path.expanduser(path)
 
     def build_model(self, args, input, ground_truth, network, loss_function,
               global_step=tf.Variable(0, trainable=False)):
@@ -37,94 +39,98 @@ class Resnet():
             # Training Configuration
             if args.gpu_id is not None:
                 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
-                sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+                self.sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
             else:
-                sess = tf.Session()
+                self.sess = tf.Session()
             # Initialize variables
-            sess.run(tf.global_variables_initializer())
-            sess.run(tf.local_variables_initializer())
-            summary_writer = tf.summary.FileWriter(args.log_dir, sess.graph)
+            self.sess.run(tf.global_variables_initializer())
+            self.sess.run(tf.local_variables_initializer())
+            summary_writer = tf.summary.FileWriter(args.log_dir, self.sess.graph)
             coord = tf.train.Coordinator()
-            tf.train.start_queue_runners(coord=coord, sess=sess)
+            tf.train.start_queue_runners(coord=coord, sess=self.sess)
 
     def fit(self, dataset):
+        dataset = load.ilsvrc_dataset(self.dataset_path)
         for i in range(self.epoch_num):
-            sess.run(enqueue_op, {image_paths_placeholder: triplet_paths_array, ground_truth_placeholder: labels_array})
+            self.sess.run(self.enqueue_op, {self.image_paths_placeholder: triplet_paths_array,
+                                            self.ground_truth_placeholder: labels_array})
 
     def evaluation(prediction, ground_truth):
         correct = tf.nn.in_top_k(predictions=prediction, targets=ground_truth, k=1)
         return tf.reduce_sum(tf.cast(correct, tf.int32))
 
-def calculate_loss(input, ground_truth):
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(input, ground_truth))
-    reg_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-    total_loss = tf.add_n([loss] + reg_loss, name='total_loss')
-    tf.summary.scalar('total_loss', total_loss)
-    return total_loss
+    @staticmethod
+    def calculate_loss(input, ground_truth):
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(input, ground_truth))
+        reg_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        total_loss = tf.add_n([loss] + reg_loss, name='total_loss')
+        tf.summary.scalar('total_loss', total_loss)
+        return total_loss
 
-def resnet_18(input):
-    net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
-    net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
+    @staticmethod
+    def resnet_18(input):
+        net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
+        net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
 
-    net = block.resnet_block(net, scope="conv2_x", filters=[64, 64], kernel_sizes=[3, 3], repeat=2)
-    net = block.resnet_block(net, scope="conv3_x", filters=[128, 128], kernel_sizes=[3, 3], repeat=2)
-    net = block.resnet_block(net, scope="conv4_x", filters=[256, 256], kernel_sizes=[3, 3], repeat=2)
-    net = block.resnet_block(net, scope="conv5_x", filters=[512, 512], kernel_sizes=[3, 3], repeat=2)
+        net = block.resnet_block(net, scope="conv2_x", filters=[64, 64], kernel_sizes=[3, 3], repeat=2)
+        net = block.resnet_block(net, scope="conv3_x", filters=[128, 128], kernel_sizes=[3, 3], repeat=2)
+        net = block.resnet_block(net, scope="conv4_x", filters=[256, 256], kernel_sizes=[3, 3], repeat=2)
+        net = block.resnet_block(net, scope="conv5_x", filters=[512, 512], kernel_sizes=[3, 3], repeat=2)
 
-    net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
-    return net
+        net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
+        return net
 
+    @staticmethod
+    def resnet_34(input):
+        net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
+        net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
 
-def resnet_34(input):
-    net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
-    net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
+        net = block.resnet_block(net, scope="conv2_x", filters=[64, 64], kernel_sizes=[3, 3], repeat=3)
+        net = block.resnet_block(net, scope="conv3_x", filters=[128, 128], kernel_sizes=[3, 3], repeat=4)
+        net = block.resnet_block(net, scope="conv4_x", filters=[256, 256], kernel_sizes=[3, 3], repeat=6)
+        net = block.resnet_block(net, scope="conv5_x", filters=[512, 512], kernel_sizes=[3, 3], repeat=3)
 
-    net = block.resnet_block(net, scope="conv2_x", filters=[64, 64], kernel_sizes=[3, 3], repeat=3)
-    net = block.resnet_block(net, scope="conv3_x", filters=[128, 128], kernel_sizes=[3, 3], repeat=4)
-    net = block.resnet_block(net, scope="conv4_x", filters=[256, 256], kernel_sizes=[3, 3], repeat=6)
-    net = block.resnet_block(net, scope="conv5_x", filters=[512, 512], kernel_sizes=[3, 3], repeat=3)
+        net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
+        return net
 
-    net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
-    return net
+    @staticmethod
+    def resnet_50(input):
+        net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
+        net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
 
+        net = block.resnet_block(net, scope="conv2_x", filters=[64, 128, 256], kernel_sizes=[1, 3, 1], repeat=3)
+        net = block.resnet_block(net, scope="conv3_x", filters=[128, 128, 256], kernel_sizes=[1, 3, 1], repeat=4)
+        net = block.resnet_block(net, scope="conv4_x", filters=[256, 256, 1024], kernel_sizes=[1, 3, 1], repeat=6)
+        net = block.resnet_block(net, scope="conv5_x", filters=[512, 512, 2048], kernel_sizes=[1, 3, 1], repeat=3)
 
-def resnet_50(input):
-    net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
-    net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
+        net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
+        return net
 
-    net = block.resnet_block(net, scope="conv2_x", filters=[64, 128, 256], kernel_sizes=[1, 3, 1], repeat=3)
-    net = block.resnet_block(net, scope="conv3_x", filters=[128, 128, 256], kernel_sizes=[1, 3, 1], repeat=4)
-    net = block.resnet_block(net, scope="conv4_x", filters=[256, 256, 1024], kernel_sizes=[1, 3, 1], repeat=6)
-    net = block.resnet_block(net, scope="conv5_x", filters=[512, 512, 2048], kernel_sizes=[1, 3, 1], repeat=3)
+    @staticmethod
+    def resnet_101(input):
+        net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
+        net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
 
-    net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
-    return net
+        net = block.resnet_block(net, scope="conv2_x", filters=[64, 128, 256], kernel_sizes=[1, 3, 1], repeat=3)
+        net = block.resnet_block(net, scope="conv3_x", filters=[128, 128, 256], kernel_sizes=[1, 3, 1], repeat=4)
+        net = block.resnet_block(net, scope="conv4_x", filters=[256, 256, 1024], kernel_sizes=[1, 3, 1], repeat=23)
+        net = block.resnet_block(net, scope="conv5_x", filters=[512, 512, 2048], kernel_sizes=[1, 3, 1], repeat=3)
 
+        net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
+        return net
 
-def resnet_101(input):
-    net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
-    net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
+    @staticmethod
+    def resnet_152(input):
+        net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
+        net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
 
-    net = block.resnet_block(net, scope="conv2_x", filters=[64, 128, 256], kernel_sizes=[1, 3, 1], repeat=3)
-    net = block.resnet_block(net, scope="conv3_x", filters=[128, 128, 256], kernel_sizes=[1, 3, 1], repeat=4)
-    net = block.resnet_block(net, scope="conv4_x", filters=[256, 256, 1024], kernel_sizes=[1, 3, 1], repeat=23)
-    net = block.resnet_block(net, scope="conv5_x", filters=[512, 512, 2048], kernel_sizes=[1, 3, 1], repeat=3)
+        net = block.resnet_block(net, scope="conv2_x", filters=[64, 128, 256], kernel_sizes=[1, 3, 1], repeat=3)
+        net = block.resnet_block(net, scope="conv3_x", filters=[128, 128, 256], kernel_sizes=[1, 3, 1], repeat=8)
+        net = block.resnet_block(net, scope="conv4_x", filters=[256, 256, 1024], kernel_sizes=[1, 3, 1], repeat=36)
+        net = block.resnet_block(net, scope="conv5_x", filters=[512, 512, 2048], kernel_sizes=[1, 3, 1], repeat=3)
 
-    net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
-    return net
-
-
-def resnet_152(input):
-    net = block.resnet_block(input, scope="conv1_x", filters=[64], kernel_sizes=[7])
-    net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 3], strides=2)
-
-    net = block.resnet_block(net, scope="conv2_x", filters=[64, 128, 256], kernel_sizes=[1, 3, 1], repeat=3)
-    net = block.resnet_block(net, scope="conv3_x", filters=[128, 128, 256], kernel_sizes=[1, 3, 1], repeat=8)
-    net = block.resnet_block(net, scope="conv4_x", filters=[256, 256, 1024], kernel_sizes=[1, 3, 1], repeat=36)
-    net = block.resnet_block(net, scope="conv5_x", filters=[512, 512, 2048], kernel_sizes=[1, 3, 1], repeat=3)
-
-    net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
-    return net
+        net = tf.layers.dense(net, 1000, activation=tf.nn.softmax, name="average_pool")
+        return net
 
 
 if __name__ == "__main__":
